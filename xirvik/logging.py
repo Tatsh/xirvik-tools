3e6bca7d4540b7d16789dacd5a0b17fa4c5d87ac
Andrew Udvare <audvare@gmail.com>
2015-04-04 18:16:55 -0700

    Replacement for lftp way; moves torrent and adds proper label

---
+++ xirvik/logging.py
@@ -0,0 +1,35 @@
+from logging.handlers import SysLogHandler
+import logging
+import sys
+
+syslogh = None
+
+def cleanup():
+    global syslogh
+
+    if syslogh:
+        syslogh.close()
+
+    logging.shutdown()
+
+def get_logger(name, level=logging.INFO, verbose=False, debug=False, syslog=False):
+    global syslogh
+
+    log = logging.getLogger(name)
+
+    if verbose or debug:
+        log.setLevel(level if not debug else logging.DEBUG)
+
+        channel = logging.StreamHandler(sys.stdout if debug else sys.stderr)
+        channel.setFormatter(logging.Formatter('%(asctime)s - %(message)s'))
+        channel.setLevel(level if not debug else logging.DEBUG)
+        log.addHandler(channel)
+
+    if syslog:
+        syslogh = SysLogHandler(address='/dev/log')
+
+        syslogh.setFormatter(logging.Formatter('%(message)s'))
+        syslogh.setLevel(logging.DEBUG if debug else logging.INFO)
+        log.addHandler(syslogh)
+
+    return log
---
+++ xirvik/util.py
@@ -0,0 +1,11 @@
+from xirvik.logging import cleanup
+import sys
+
+def cleanup_and_exit(status=0):
+    cleanup()
+    sys.exit(status)
+
+
+def ctrl_c_handler(signum, frame):
+    cleanup()
+    raise SystemExit('Signal raised')
