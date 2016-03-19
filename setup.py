4818c84308ecb89f7f12c8e77db592e6d1dea4cd
Andrew Udvare <audvare@gmail.com>
2016-03-18 20:24:26 -0700

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='xirvik-tools',
-    version='0.0.4',
+    version='0.0.5',
     author='Fa An',
     author_email='2998784916@qq.com',
     packages=['xirvik', 'xirvik.client'],
