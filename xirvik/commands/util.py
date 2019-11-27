bf065aa5813920404dfb2e90a214d15f1173d96f
Andrew Udvare <audvare@gmail.com>
2019-11-27 00:24:35 -0500

    Common argument parser function

--- xirvik/commands/util.py
+++ xirvik/commands/util.py
@@ -1,5 +1,6 @@
 from os.path import basename
 from typing import Optional
+import argparse
 import logging
 import sys
 
@@ -14,3 +15,31 @@ def setup_logging_stdout(name: Optional[str] = None,
     channel.setLevel(logging.DEBUG if verbose else logging.INFO)
     log.addHandler(channel)
     return log
+
+
+def common_parser() -> argparse.ArgumentParser:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-u', '--username', required=False, help='Xirvik user')
+    parser.add_argument('-p',
+                        '--password',
+                        required=False,
+                        help='Xirvik password')
+    parser.add_argument(
+        '-r',
+        '--max-retries',
+        type=int,
+        default=10,
+        help='Number of retries for each request (passed to client)')
+    parser.add_argument('-v',
+                        '--verbose',
+                        action='store_true',
+                        help='Enable verbose logging')
+    parser.add_argument(
+        '--backoff-factor',
+        type=int,
+        default=5,
+        help=('Back-off factor used when calculating time to wait to retry '
+              'a failed request'))
+    parser.add_argument('--netrc', required=False, help='netrc file path')
+    parser.add_argument('host', nargs=1, help='Host name')
+    return parser
