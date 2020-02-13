93f4756f4bd7a5491a21140a5a3c856b13c5ed0b
Andrew Udvare <audvare@gmail.com>
2020-02-12 21:12:49 -0500

    xirvik/log: clean up

--- xirvik/log.py
+++ xirvik/log.py
@@ -8,12 +8,8 @@ syslogh = None
 
 def cleanup() -> None:
     """Close syslog handle and calls logging.shutdown()."""
-    global syslogh
-
     if syslogh:
         syslogh.close()
-
-    logging.shutdown()
 
 
 def get_logger(name: str,
@@ -27,23 +23,17 @@ def get_logger(name: str,
     Return a logger object.
     """
     global syslogh
-
     log = logging.getLogger(name)
-
     if verbose or debug:
         log.setLevel(level if not debug else logging.DEBUG)
-
         channel = logging.StreamHandler(sys.stdout if debug else sys.stderr)
         channel.setFormatter(logging.Formatter('%(levelname)s - %(message)s'))
         channel.setLevel(level if not debug else logging.DEBUG)
         log.addHandler(channel)
-
     if syslog:
         log.setLevel(level)
         syslogh = SysLogHandler(address='/dev/log')
-
         syslogh.setFormatter(logging.Formatter('%(message)s'))
         syslogh.setLevel(logging.INFO)
         log.addHandler(syslogh)
-
     return log
