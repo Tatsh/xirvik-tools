24f9f1b2ddd21d5ff44bc87cb9c80e32f628b978
Andrew Udvare <audvare@gmail.com>
2020-02-13 00:59:38 -0500

    xirvik/util: add docstrings

--- xirvik/util.py
+++ xirvik/util.py
@@ -24,28 +24,25 @@ T = TypeVar('T')
 
 
 class ReadableDirectoryAction(argparse.Action):
+    """Checks if a directory argument is a directory and is readable."""
     def __call__(self,
                  parser: argparse.ArgumentParser,
                  namespace: argparse.Namespace,
                  values: Optional[Union[str, Sequence[Any]]],
                  option_string: Optional[str] = None) -> None:
         prospective_dir = values
-
         if not isdir(cast(str, prospective_dir)):
             raise argparse.ArgumentTypeError('%s is not a valid directory' %
                                              (prospective_dir, ))
-
         # Since macOS 10.15, the Python binary will need access to this
         # directory and a prompt from TCC must appear for this to work
         # Because of TCC becoming more strict, hecking with access() is not
         # reliable on macOS
         if platform.system() == 'Darwin':
             return
-
         if access(cast(str, prospective_dir), R_OK):
             setattr(namespace, self.dest, realpath(cast(str, prospective_dir)))
             return
-
         username = environ['USER']
         raise argparse.ArgumentTypeError(
             f'{prospective_dir} is not a readable directory (checking as user '
@@ -53,6 +50,10 @@ class ReadableDirectoryAction(argparse.Action):
 
 
 class ReadableDirectoryListAction(argparse.Action):
+    """
+    Checks if a list of directories argument is a directory and all are
+    readable..
+    """
     def __call__(self,
                  parser: argparse.ArgumentParser,
                  namespace: argparse.Namespace,
