b08160ad8302929b19b34d827d1a38b2f4241ea7
Andrew Udvare <audvare@gmail.com>
2020-02-20 01:27:27 -0500

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='xirvik-tools',
-    version='0.2.2',
+    version='0.3.0',
     author='Fa An',
     author_email='2998784916@qq.com',
     packages=['xirvik', 'xirvik.commands', 'xirvik.test'],
