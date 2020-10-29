db58972a404fc570207140da68a5329862e57737
Andrew Udvare <audvare@gmail.com>
2020-10-29 06:10:29 -0400

    xirvik/commands/__init__: remove old import

--- xirvik/commands/__init__.py
+++ xirvik/commands/__init__.py
@@ -1,8 +1,6 @@
 """Command exports."""
 from .simple import (add_ftp_user, authorize_ip, delete_ftp_user, fix_rtorrent,
-                     mirror_main, start_torrents)
+                     start_torrents)
 
-__all__ = [
-    'add_ftp_user', 'authorize_ip', 'delete_ftp_user', 'fix_rtorrent',
-    'mirror_main', 'start_torrents'
-]
+__all__ = ('add_ftp_user', 'authorize_ip', 'delete_ftp_user', 'fix_rtorrent',
+           'start_torrents')
