7fb6bda6b2e2acd7c2952deaabcc9727c6a1c0e4
Andrew Udvare <audvare@gmail.com>
2020-12-01 17:12:08 -0500

    xirvik/commands/move_erroneous: fix type issues

--- xirvik/commands/move_erroneous.py
+++ xirvik/commands/move_erroneous.py
@@ -1,6 +1,8 @@
 """Move torrents in error state to another location."""
 from time import sleep
-from typing import Iterable, List, Tuple, TypeVar
+from typing import Iterable, List, Tuple, TypeVar, cast
+import argparse
+import logging
 import sys
 
 from typing_extensions import Final
@@ -10,10 +12,10 @@ from ..client import ruTorrentClient
 from ..typing import TorrentDict
 from .util import common_parser, setup_logging_stdout
 
-__all__ = ("main", )
+__all__ = ('main',)
 
-PREFIX: Final = '/torrents/{}/_completed-not-active'
-BAD_MESSAGES: Final = (
+PREFIX: Final[str] = '/torrents/{}/_completed-not-active'
+BAD_MESSAGES: Final[Tuple[str, ...]] = (
     'unregistered torrent',
     "couldn't connect to server",
     'server returned nothing',
@@ -28,7 +30,10 @@ def _has_one(look_for_list: Iterable[T], val: Iterable[T]) -> bool:
     return False
 
 
-def _should_process(x: TorrentDict) -> bool:
+def _should_process(x: TorrentDict, log: logging.Logger) -> bool:
+    log.debug(
+        'Name: "%s", message: "%s", is_hash_checking: %s, left_bytes: %d',
+        x['name'], x['message'], x['is_hash_checking'], x['left_bytes'])
     return (_has_one(BAD_MESSAGES, x['message'].lower())
             and not x['is_hash_checking'] and x['left_bytes'] == 0)
 
@@ -37,24 +42,33 @@ def _make_move_to(prefix: str, label: str) -> str:
     return '{}/{}'.format(prefix, label)
 
 
+class Namespace(argparse.Namespace):
+    """Arguments."""
+    ignore_ratio: bool
+    sleep_time: int
+    verbose: bool
+
+
 def main() -> int:
     """Move torrents in error state to another location."""
-    parser: Final = common_parser()
+    parser: Final[argparse.ArgumentParser] = common_parser()
     parser.add_argument('-a', '--ignore-ratio', action='store_true')
     parser.add_argument('-t', '--sleep-time', default=10, type=int)
     argcomplete.autocomplete(parser)
-    args: Final = parser.parse_args()
-    log: Final = setup_logging_stdout(verbose=args.verbose)
-    client: Final = ruTorrentClient(args.host[0],
-                                    name=args.username,
-                                    password=args.password,
-                                    max_retries=args.max_retries,
-                                    netrc_path=args.netrc)
-    prefix: Final = PREFIX.format(client.name)
+    args: Final[Namespace] = cast(Namespace, parser.parse_args())
+    log: Final[logging.Logger] = setup_logging_stdout(verbose=args.verbose)
+    client: Final[ruTorrentClient] = ruTorrentClient(
+        args.host[0],
+        name=args.username,
+        password=args.password,
+        max_retries=args.max_retries,
+        netrc_path=args.netrc)
+    prefix: Final[str] = PREFIX.format(client.name)
     to_delete: List[Tuple[str, str]] = []
-    items: Final = [(hash_, info)
-                    for hash_, info in client.list_torrents_dict().items()
-                    if _should_process(info)]
+    items: Final[List[Tuple[str, TorrentDict]]] = [
+        (hash_, info) for hash_, info in client.list_torrents_dict().items()
+        if _should_process(info, log)
+    ]
     count = 0
     for hash_, info in items:
         log.info('Stopping %s', info['name'])
