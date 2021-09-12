0113bcfda536bdfd911d6c74d7dd0b06e2b8d255
Andrew Udvare <audvare@gmail.com>
2021-09-11 22:46:07 -0400

    xirvik/commands/tests/test_simple: create

---
+++ xirvik/commands/tests/test_simple.py
@@ -0,0 +1,19 @@
+from click.testing import CliRunner
+import pytest
+import requests_mock as req_mock
+
+from ..root import xirvik
+
+# pylint: disable=missing-function-docstring,protected-access,no-self-use
+
+
+@pytest.fixture()
+def runner():
+    return CliRunner()
+
+
+def test_fix_rtorrent(requests_mock: req_mock.Mocker, runner: CliRunner):
+    requests_mock.get('https://somehost.com:443/userpanel/index.php/services/'
+                      'restart/rtorrent')
+    result = runner.invoke(xirvik, ['rtorrent', 'fix', 'somehost.com'])
+    assert result.exit_code == 0
