b6f4d37f1784064c0d2db3b31ff574289d5d8759
Andrew Udvare <audvare@gmail.com>
2021-09-14 01:10:53 -0400

    xirvik/commands/move_by_label: port to Click

--- xirvik/commands/move_by_label.py
+++ xirvik/commands/move_by_label.py
@@ -1,27 +1,28 @@
 #!/usr/bin/env python
-# PYTHON_ARGCOMPLETE_OK
 """Organise torrents based on labels assigned in ruTorrent."""
+from os.path import expanduser
 from time import sleep
-from typing import Any, Callable, Tuple
-import sys
+from typing import Any, Callable, Optional, Sequence, Tuple
 
+from loguru import logger
 from requests.exceptions import HTTPError
-import argcomplete
+import click
 
 from xirvik.typing import TorrentDict
 
 from ..client import ruTorrentClient
-from .util import common_parser, setup_logging_stdout
+from .util import common_options_and_arguments, setup_log_intercept_handler
 
-PREFIX = '/torrents/{}/_completed'
+PREFIX = '/torrents/{}/{}'
 
 
 def _base_path_check(
-        username: str) -> Callable[[Tuple[Any, TorrentDict]], bool]:
+        username: str,
+        completed_dir: str) -> Callable[[Tuple[Any, TorrentDict]], bool]:
     def bpc(hash_info: Tuple[Any, TorrentDict]) -> bool:
         _, info = hash_info
-        move_to = '{}/{}'.format(PREFIX.format(username),
-                                 str.lower(info['custom1']))
+        move_to = '{}/{}'.format(PREFIX.format(username, completed_dir),
+                                 str.lower(info['custom1'] or ''))
         return not info['base_path'].startswith(move_to)
 
     return bpc
@@ -32,64 +33,69 @@ def _key_check(hash_info: Tuple[Any, TorrentDict]) -> bool:
     return not info['is_hash_checking'] and info['left_bytes'] == 0
 
 
-def main() -> int:
-    """Entry point."""
-    parser = common_parser()
-    parser.add_argument(
-        '-c',
-        '--completed-dir',
-        default='_completed',
-        help='Top directory where moved torrent data will be placed')
-    parser.add_argument(
-        '-t',
-        '--sleep-time',
-        default=10,
-        type=int,
-        help=('Time to sleep in seconds at certain times during this batch '
-              'of requests'))
-    parser.add_argument(
-        '-l',
-        '--lower-label',
-        action='store_true',
-        help='Call lower() on labels used to make directory names')
-    parser.add_argument('--ignore-labels',
-                        nargs='+',
-                        default=[],
-                        help='List of label names to ignore')
-    argcomplete.autocomplete(parser)
-    args = parser.parse_args()
-    log = setup_logging_stdout(verbose=args.verbose)
-    assert log is not None
-    client = ruTorrentClient(args.host[0],
-                             name=args.username,
-                             password=args.password,
-                             max_retries=args.max_retries,
-                             netrc_path=args.netrc)
+@click.command()
+@common_options_and_arguments
+@click.option('-c',
+              '--completed-dir',
+              default='_completed',
+              help='Top directory where moved torrent data will be placed')
+@click.option(
+    '-t',
+    '--sleep-time',
+    default=10,
+    type=int,
+    help=('Time to sleep in seconds at certain times during this batch '
+          'of requests'))
+@click.option('-l',
+              '--lower-label',
+              is_flag=True,
+              help='Call lower() on labels used to make directory names')
+@click.option('--ignore-labels',
+              multiple=True,
+              default=[],
+              help='List of labels to ignore (case-sensitive)')
+def main(host: str,
+         ignore_labels: Sequence[str],
+         netrc: Optional[str] = None,
+         username: Optional[str] = None,
+         password: Optional[str] = None,
+         completed_dir: str = '_completed',
+         sleep_time: int = 10,
+         lower_label: bool = False,
+         max_retries: int = 10,
+         debug: bool = False,
+         backoff_factor: int = 1) -> None:
+    """Move torrents according to labels assigned."""
+    if debug:
+        setup_log_intercept_handler()
+        logger.enable('')
+    else:
+        logger.level('INFO')
+    client = ruTorrentClient(host,
+                             name=username,
+                             password=password,
+                             max_retries=max_retries,
+                             netrc_path=netrc or expanduser('~/.netrc'),
+                             backoff_factor=backoff_factor)
     username = client.name
-    try:
-        torrents = list(client.list_torrents_dict().items())
-    except (ValueError, HTTPError):
-        log.error('Connection failed on list_torrents() call')
-        return 1
-    count = 0
     assert username is not None
-    hash_: str
-    info: TorrentDict
-    for hash_, info in list(
-            filter(_base_path_check(username), filter(_key_check, torrents))):
+    try:
+        torrents = client.list_torrents_dict()
+    except (ValueError, HTTPError) as e:
+        logger.error('Connection failed on list_torrents() call')
+        raise click.Abort() from e
+    count = 0
+    for hash_, info in (y
+                        for y in (x for x in torrents.items() if _key_check(x))
+                        if _base_path_check(username, completed_dir)(y)):
         label = info['custom1']
-        if label in args.ignore_labels:
+        if not label or label in ignore_labels:
             continue
-        if args.lower_label:
+        if lower_label:
             label = label.lower()
-        move_to = '{}/{}'.format(PREFIX.format(username), label)
-        log.info('Moving %s to %s/', info['name'], move_to)
+        move_to = '{}/{}'.format(PREFIX.format(username, completed_dir), label)
+        logger.info(f'Moving {info["name"]} to {move_to}/')
         client.move_torrent(hash_, move_to)
         count += 1
         if count > 0 and (count % 10) == 0:
-            sleep(args.sleep_time)
-    return 0
-
-
-if __name__ == '__main__':
-    sys.exit(main())
+            sleep(sleep_time)
