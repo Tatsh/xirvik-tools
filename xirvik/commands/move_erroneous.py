8c3673bff97aa790ba9663e670241839a17df19f
Andrew Udvare <audvare@gmail.com>
2021-09-14 01:10:48 -0400

    xirvik/commands/move_erroneous: port to Click

--- xirvik/commands/move_erroneous.py
+++ xirvik/commands/move_erroneous.py
@@ -1,16 +1,13 @@
 """Move torrents in error state to another location."""
 from time import sleep
-from typing import Iterable, List, Tuple, TypeVar, cast
-import argparse
-import logging
-import sys
+from typing import Final, Iterable, List, Optional, Tuple, TypeVar
 
-from typing_extensions import Final
-import argcomplete
+from loguru import logger
+import click
 
 from ..client import ruTorrentClient
 from ..typing import TorrentDict
-from .util import common_parser, setup_logging_stdout
+from .util import common_options_and_arguments, setup_log_intercept_handler
 
 __all__ = ('main',)
 
@@ -30,71 +27,70 @@ def _has_one(look_for_list: Iterable[T], val: Iterable[T]) -> bool:
     return False
 
 
-def _should_process(x: TorrentDict, log: logging.Logger) -> bool:
-    log.debug(
-        'Name: "%s", message: "%s", is_hash_checking: %s, left_bytes: %d',
-        x['name'], x['message'], x['is_hash_checking'], x['left_bytes'])
+def _should_process(x: TorrentDict) -> bool:
+    logger.debug(f'Name: "{x["name"]}", message: "{x["message"]}", '
+                 f'is_hash_checking: {x["is_hash_checking"]}, '
+                 f'left_bytes: {x["left_bytes"]}')
     return (_has_one(BAD_MESSAGES, x['message'].lower())
-            and not x['is_hash_checking'] and x['left_bytes'] == 0)
+            and not x['is_hash_checking'] and x['left_bytes'] == 0
+            and bool(x['custom1']))
 
 
 def _make_move_to(prefix: str, label: str) -> str:
     return '{}/{}'.format(prefix, label)
 
 
-class Namespace(argparse.Namespace):  # pylint: disable=too-few-public-methods
-    """Arguments."""
-    ignore_ratio: bool
-    sleep_time: int
-    verbose: bool
-
-
-def main() -> int:
+@click.command()
+@common_options_and_arguments
+@click.option('-a', '--ignore-ratio', is_flag=True)
+@click.option('--sleep-time', type=int, default=10)
+def main(
+    host: str,
+    netrc: Optional[str] = None,
+    username: Optional[str] = None,
+    password: Optional[str] = None,
+    sleep_time: int = 10,
+    debug: bool = False,
+    max_retries: int = 10,
+) -> int:
     """Move torrents in error state to another location."""
-    parser: Final[argparse.ArgumentParser] = common_parser()
-    parser.add_argument('-a', '--ignore-ratio', action='store_true')
-    parser.add_argument('-t', '--sleep-time', default=10, type=int)
-    argcomplete.autocomplete(parser)
-    args: Final[Namespace] = cast(Namespace, parser.parse_args())
-    log: Final[logging.Logger] = setup_logging_stdout(verbose=args.verbose)
-    client: Final[ruTorrentClient] = ruTorrentClient(
-        args.host[0],
-        name=args.username,
-        password=args.password,
-        max_retries=args.max_retries,
-        netrc_path=args.netrc)
-    prefix: Final[str] = PREFIX.format(client.name)
+    if debug:
+        setup_log_intercept_handler()
+        logger.enable('')
+    else:
+        logger.level('INFO')
+    client = ruTorrentClient(host,
+                             name=username,
+                             password=password,
+                             max_retries=max_retries,
+                             netrc_path=netrc)
+    prefix = PREFIX.format(client.name)
     to_delete: List[Tuple[str, str]] = []
-    items: Final[List[Tuple[str, TorrentDict]]] = [
-        (hash_, info) for hash_, info in client.list_torrents_dict().items()
-        if _should_process(info, log)
-    ]
+    items = [(hash_, info)
+             for hash_, info in client.list_torrents_dict().items()
+             if _should_process(info)]
     count = 0
     for hash_, info in items:
-        log.info('Stopping %s', info['name'])
+        logger.info(f'Stopping {info["name"]}')
         client.stop(hash_)
         count += 1
         if count > 0 and (count % 10) == 0:
-            sleep(args.sleep_time)
+            sleep(sleep_time)
     count = 0
     for hash_, info in items:
         move_to = _make_move_to(prefix, info['custom1'].lower())
         to_delete.append((hash_, info['name']))
-        log.info('Moving %s to %s/', info['name'], move_to)
+        logger.info(f'Moving {info["name"]} to {move_to}/')
         client.move_torrent(hash_, move_to)
         client.stop(hash_)
         count += 1
         if count > 0 and (count % 10) == 0:
-            sleep(args.sleep_time)
+            sleep(sleep_time)
     count = 0
     for hash_, name in to_delete:
-        log.info('Removing torrent "%s" (without deleting data)', name)
+        logger.info(f'Removing torrent "{name}" (without deleting data)')
         client.remove(hash_)
         count += 1
         if count > 0 and (count % 10) == 0:
-            sleep(args.sleep_time)
+            sleep(sleep_time)
     return 0
-
-
-if __name__ == '__main__':
-    sys.exit(main())
