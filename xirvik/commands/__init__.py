ffdc6cf42eb4118a137cf596ce93d81749d6f0b0
Andrew Udvare <audvare@gmail.com>
2021-09-14 01:11:32 -0400

    xirvik/commands/__init__: only export xirvik

--- xirvik/commands/__init__.py
+++ xirvik/commands/__init__.py
@@ -1,7 +1,4 @@
 """Command exports."""
-from .simple import (add_ftp_user, authorize_ip, delete_ftp_user, fix_rtorrent,
-                     start_torrents)
 from .root import xirvik
 
-__all__ = ('add_ftp_user', 'authorize_ip', 'delete_ftp_user', 'fix_rtorrent',
-           'start_torrents', 'xirvik')
+__all__ = ('xirvik',)
