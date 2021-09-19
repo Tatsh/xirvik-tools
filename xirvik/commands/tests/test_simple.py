8f504ad0665fb78d880cbc4d0375dbf50372d316
Andrew Udvare <audvare@gmail.com>
2021-09-19 00:38:33 -0400

    xirvik/commands/tests/test_simple: finish

--- xirvik/commands/tests/test_simple.py
+++ xirvik/commands/tests/test_simple.py
@@ -33,15 +33,6 @@ def test_fix_rtorrent_fail(requests_mock: req_mock.Mocker, runner: CliRunner):
                          ('rtorrent', 'fix', 'somehost.com')).exit_code == 1
 
 
-def test_start_torrents_no_auth(runner: CliRunner, tmp_path: pathlib.Path):
-    netrc = tmp_path / '.netrc'
-    netrc.write_text('machine machine.com login somename password pass\n')
-    os.environ['HOME'] = str(tmp_path)
-    assert runner.invoke(
-        xirvik,
-        ('rtorrent', 'add', 'machine1.com', expanduser('~'))).exit_code != 0
-
-
 def test_start_torrents_zero_files(runner: CliRunner, tmp_path: pathlib.Path):
     netrc = tmp_path / '.netrc'
     netrc.write_text('machine machine.com login somename password pass\n')
@@ -115,3 +106,85 @@ def test_start_torrents_start_stopped(runner: CliRunner,
     assert not torrent.is_file()
     assert (m.last_request and m.last_request.text and
             'name="torrents_start_stopped"\r\n\r\non' in m.last_request.text)
+
+
+def test_add_ftp_user(runner: CliRunner, requests_mock: req_mock.Mocker,
+                      tmp_path: pathlib.Path):
+    netrc = tmp_path / '.netrc'
+    netrc.write_text('machine machine.com login somename password pass\n')
+    os.environ['HOME'] = str(tmp_path)
+    m = requests_mock.post(
+        'https://machine.com:443/userpanel/index.php/ftp_users/add_user')
+    assert runner.invoke(xirvik, ('ftp', 'add-user', 'machine.com', 'newuser',
+                                  'newpass')).exit_code == 0
+    assert m.called_once is True
+
+
+def test_add_ftp_user_error(runner: CliRunner, requests_mock: req_mock.Mocker,
+                            tmp_path: pathlib.Path):
+    netrc = tmp_path / '.netrc'
+    netrc.write_text('machine machine.com login somename password pass\n')
+    os.environ['HOME'] = str(tmp_path)
+    m = requests_mock.post(
+        'https://machine.com:443/userpanel/index.php/ftp_users/add_user',
+        status_code=500)
+    assert runner.invoke(xirvik, ('ftp', 'add-user', 'machine.com', 'newuser',
+                                  'newpass')).exit_code != 0
+    assert m.called_once is True
+
+
+def test_delete_ftp_user(runner: CliRunner, requests_mock: req_mock.Mocker,
+                         tmp_path: pathlib.Path):
+    netrc = tmp_path / '.netrc'
+    netrc.write_text('machine machine.com login somename password pass\n')
+    os.environ['HOME'] = str(tmp_path)
+    m = requests_mock.get(
+        ('https://machine.com:443/userpanel/index.php/ftp_users/delete/'
+         'bmV3dXNlcg=='))
+    assert runner.invoke(
+        xirvik,
+        ('ftp', 'delete-user', 'machine.com', 'newuser')).exit_code == 0
+    assert m.called_once is True
+
+
+def test_delete_ftp_user_error(runner: CliRunner,
+                               requests_mock: req_mock.Mocker,
+                               tmp_path: pathlib.Path):
+    netrc = tmp_path / '.netrc'
+    netrc.write_text('machine machine.com login somename password pass\n')
+    os.environ['HOME'] = str(tmp_path)
+    m = requests_mock.get(
+        ('https://machine.com:443/userpanel/index.php/ftp_users/delete/'
+         'bmV3dXNlcg=='),
+        status_code=500)
+    assert runner.invoke(
+        xirvik,
+        ('ftp', 'delete-user', 'machine.com', 'newuser')).exit_code != 0
+    assert m.called_once is True
+
+
+def test_authorize_ip(runner: CliRunner, requests_mock: req_mock.Mocker,
+                      tmp_path: pathlib.Path):
+    netrc = tmp_path / '.netrc'
+    netrc.write_text('machine machine.com login somename password pass\n')
+    os.environ['HOME'] = str(tmp_path)
+    m = requests_mock.get(
+        ('https://machine.com:443/userpanel/index.php/virtual_machine/'
+         'authorize_ip'))
+    assert runner.invoke(xirvik,
+                         ('vm', 'authorize-ip', 'machine.com')).exit_code == 0
+    assert m.called_once is True
+
+
+def test_authorize_ip_error(runner: CliRunner, requests_mock: req_mock.Mocker,
+                            tmp_path: pathlib.Path):
+    netrc = tmp_path / '.netrc'
+    netrc.write_text('machine machine.com login somename password pass\n')
+    os.environ['HOME'] = str(tmp_path)
+    m = requests_mock.get(
+        ('https://machine.com:443/userpanel/index.php/virtual_machine/'
+         'authorize_ip'),
+        status_code=500)
+    assert runner.invoke(xirvik,
+                         ('vm', 'authorize-ip', 'machine.com')).exit_code != 0
+    assert m.called_once is True
