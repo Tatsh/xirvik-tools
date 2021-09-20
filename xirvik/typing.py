d0875d6795d4cef7652a00ec3aada953f66d91f7
Andrew Udvare <audvare@gmail.com>
2021-09-20 03:52:52 -0400

    xirvik/typing: docs

--- xirvik/typing.py
+++ xirvik/typing.py
@@ -15,36 +15,70 @@ Method1 = Callable[[T, U], V]
 
 class TorrentDict(TypedDict):
     """Torrent information."""
+    #: Path before the torrent directory or file.
     base_path: str
+    #:
     bytes_done: int
+    #: Chunk size (usually a power of 2).
     chunk_size: int
+    #:
     chunks_hashed: int
+    #:
     completed_chunks: int
+    #: Date torrent was added to the client. Can be ``None`` if this was not
+    #: captured, or possibly due to a crash.
     creation_date: Optional[datetime]
+    #: Usually contains the assigned label.
     custom1: str
+    #:
     custom2: str
+    #:
     down_rate: int
+    #:
     free_diskspace: int
+    #:
     hashing: bool
+    #:
     is_active: int
+    #:
     is_hash_checked: bool
+    #:
     is_hash_checking: bool
+    #:
     is_multi_file: bool
+    #: Unknown.
     is_open: bool
+    #:
     is_private: bool
+    #:
     left_bytes: int
+    #: Message from the server.
     message: str
+    #:
     name: str
+    #:
     peers_accounted: int
+    #:
     peers_connected: int
+    #:
     peers_not_connected: int
+    #:
     priority: int
+    #:
     ratio: float
+    #:
     size_bytes: int
+    #:
     size_chunks: int
+    #:
     skip_total: int
+    #:
     state: int
+    #:
     state_changed: datetime
+    #:
     tracker_focus: int
+    #:
     up_rate: int
+    #:
     up_total: int
